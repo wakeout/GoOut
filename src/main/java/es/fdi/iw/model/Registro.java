02424dfe3d9b2b1bcdc6b5a360964428ea02836f
marquitos855 <mlaina@ucm.es>
2015-12-15 02:08:30 +0100

    registro y manytoone comentados porque petan

---
+++ src/main/java/es/fdi/iw/model/Registro.java
@@ -0,0 +1,29 @@
+package es.fdi.iw.model;
+
+
+import javax.persistence.ManyToOne;
+
+public class Registro {
+	Usuario u;
+	Actividad a;
+	
+	@ManyToOne(targetEntity=Usuario.class)
+	public Usuario getU() {
+		return u;
+	}
+	public void setU(Usuario u) {
+		this.u = u;
+	}
+	
+	@ManyToOne(targetEntity=Actividad.class)	
+	public Actividad getA() {
+		return a;
+	}
+	public void setA(Actividad a) {
+		this.a = a;
+	}
+	
+	
+	
+	
+}
